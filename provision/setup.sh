3550a14726eed690b4b3efb7f31f8cacae0edef9
Bruno Paz <brunopaz@sapo.pt>
2020-05-22 22:24:22 +0100

    updates for fedora 32

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,16 +1,28 @@
-
+#!/usr/bin/env sh
 set -e
 
+trap "exit" INT
+
+NC='\033[0m'
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+
+cat << "EOF"
+   _________       __
+ /   _____/ _____/  |_ __ ________
+ \_____  \_/ __ \   __\  |  \____ \
+ /        \  ___/|  | |  |  /  |_> >
+/_______  /\___  >__| |____/|   __/
+        \/     \/           |__|
+EOF
+
+echo -e "${Yellow}Updating base system and installing ansible and dependencies${NC}"
+
 ## Installs Ansible
-sudo dnf update && sudo dnf install -y ansible make pip3
+sudo dnf -y update && sudo dnf install -y ansible make
 
-ansible --version
-
-sudo pip3 install -r requirements.txt
-
-make galaxy
-
-make play
+make setup
 
 exec zsh
 
