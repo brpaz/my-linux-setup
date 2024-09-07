2c09e16398d4e53059a56e4d508e03e67758747a
Bruno Paz <184563+brpaz@users.noreply.github.com>
2024-09-07 14:57:29 +0100

    update docker

--- scripts/gen-mkdcert-certs.sh
+++ scripts/gen-mkdcert-certs.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
-# ======================================================================
-# This script generates mkcert certificates and configure Traefik to use them
-# ======================================================================
+# Generate certificates for the traefik proxy
+# This script uses mkcert to generate the certificates
+# To make traefik use the certificates, you need to to configure the `roles/traefik/files/conf/tls.yaml` file
+CERTS_PATH="roles/traefik/files/certs"
 
+SITES=(
+    "traefik.localhost"
+    "grafana.localhost"
+    "prometheus.localhost"
+    "portainer.localhost"
+    "sourcegraph.localhost"
+)
 
-mkcert -install
+# ENsure we are in the root directory
+cd "$(dirname "$0")/../${CERTS_PATH}"
 
-mkcert *.internal
+# Check if mkcert is installed
+if ! command -v mkcert &> /dev/null
+then
+    echo "mkcert could not be found. Please follow the instructions at https://github.com/FiloSottile/mkcert?tab=readme-ov-file#installation"
+    exit 1
+fi
 
-
-# restart traeifk
+for SITE in "${SITES[@]}"
+do
+    mkcert "${SITE}"
+done
 
